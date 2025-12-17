9087c596c03999d95dba3ace7891b2b46d22296c
shabani17 <hamidehshabani918717@gmail.com>
2025-12-17 12:16:03 +0330

    Clean up and organize API routes

--- routes/api.php
+++ routes/api.php
@@ -6,27 +6,29 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
 
-
+// Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-
+// Articles - Public
 Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/{article}', [ArticleController::class, 'show']);
 
-
+// Authenticated routes
 Route::middleware('auth:sanctum')->group(function () {
+    // Auth
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Articles
     Route::post('/articles', [ArticleController::class, 'store']);
     Route::put('/articles/{article}', [ArticleController::class, 'update']);
     Route::delete('/articles/{article}', [ArticleController::class, 'destroy']);
+
+    // Comments
+    Route::post('/articles/{article}/comments', [CommentController::class, 'store']);
+    Route::put('/comments/{comment}', [CommentController::class, 'update']);
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
 });
 
-
-
-
-
+// Comments - Public
 Route::get('/articles/{article}/comments', [CommentController::class, 'index']);
-Route::post('/articles/{article}/comments', [CommentController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/comments/{comment}', [CommentController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->middleware('auth:sanctum');
